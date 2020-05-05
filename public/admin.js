bf1f3bca58eafe23fbc0cf65d5cd2e5aa10488d6
bharatrathore <bsrathore0099@gmail.com>
2020-05-05 22:55:44 +0530

    changed modal,minor fixes

--- public/admin.js
+++ public/admin.js
@@ -1,25 +1,23 @@
-const fetchBtn=document.getElementById('fetch-data')
-const tableBody=document.getElementById('table-body')
-let c=0;
-fetchBtn.addEventListener('click',e=>{
-    fetchBtn.style.pointerEvents="none"
-    tableBody.innerHTML=''
-    fetch('../getinfo')
-    .then(data=>{
-        return data.json()
+const fetchBtn = document.getElementById("fetch-data");
+const tableBody = document.getElementById("table-body");
+let c = 0;
+fetchBtn.addEventListener("click", (e) => {
+  fetchBtn.style.pointerEvents = "none";
+  tableBody.innerHTML = "";
+  fetch("../getinfo")
+    .then((data) => {
+      return data.json();
     })
-    .then(fulldata=>{
-        let bdginfo={}
-        fulldata.forEach(user => {
-            console.log(user)
-           let tr=document.createElement("tr")
-           tr.classList.add('user-row')
-           tr.setAttribute("data-toggle","modal")
-           tr.setAttribute("data-target","#exampleModal")
-           
-
-
-           tr.innerHTML=`
+    .then((fulldata) => {
+      let bdginfo = {};
+      fulldata.forEach((user) => {
+        console.log(user);
+        let tr = document.createElement("tr");
+        tr.classList.add("user-row");
+        tr.setAttribute("data-toggle", "modal");
+        tr.setAttribute("data-target", "#exampleModal");
+        tr.style.cursor="pointer"
+        tr.innerHTML = `
             <td>${user.prn_no}</td>
             <td>${user.full_name}</td>
             <td>${user.course}</td>
@@ -27,19 +25,30 @@ fetchBtn.addEventListener('click',e=>{
             <td>${user.badgeInfo.totalStars}</td>
             <td class="d-none">${user._id}</td>
            
-           `
-        bdginfo[user._id]=user.badgeInfo.badges
-        tableBody.appendChild(tr)
-        fetchBtn.style.pointerEvents="auto"
-        tr.addEventListener('click',e=>{
-            let id=$(e.target).siblings()[4].innerText
+           `;
+        bdginfo[user._id] = user.badgeInfo.badges;
+        tableBody.appendChild(tr);
+        fetchBtn.style.pointerEvents = "auto";
+        tr.addEventListener("click", (e) => {
+          let id = $(e.target).siblings()[4].innerText;
+          let badgeDetails=bdginfo[id]
+          count=1
+          let html=""
+          Object.entries(badgeDetails).map(info=>{
+            html+=`
+                <tr>
+                <td>${count++}</td>
+                <td>${info[0]}</td>
+                <td>${info[1]}</td>
+                </tr>
+
             
-        })
-
+            `
+          })
+          document.getElementById('bdg-body').innerHTML=html
         });
-    })
-})
+      });
+    });
+});
 
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+
