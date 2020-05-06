e4db856e3db69c4e782197d594a9ea43d5494986
bharatrathore <bsrathore0099@gmail.com>
2020-05-06 18:26:22 +0530

    added login route

---
+++ routes/login.js
@@ -0,0 +1,29 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const login=require('../models/login')
+const axios=require('axios')
+const router=express.Router()
+
+router.post('/',(req,res)=>{
+    userid=req.body.userid
+    password=req.body.password
+    login.find({
+        $and:[
+            {userid:userid},
+            {password:password}
+        ]
+    }).count().then(count=>{
+        if(count!=0){
+            res.send({
+                valid:true
+            })
+        }
+        else{
+            res.send({
+                valid:false
+            })
+        }
+    })
+})
+
+module.exports=router
