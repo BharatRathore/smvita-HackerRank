1ef75f9855ad73ae7e8638468606447aa02a5b7d
bharatrathore <bsrathore0099@gmail.com>
2020-05-11 10:32:50 +0530

    changed mongoose model, added cron schedule

--- cron/dbupdate.js
+++ cron/dbupdate.js
@@ -1,51 +1,70 @@
 var cron = require('node-cron');
-const mongoose = require('mongoose')
-const fetch=require('node-fetch')
+//const mongoose = require('mongoose')
+const _=require('underscore')
 
-const hidUpdate=new mongoose.Schema({hackerRankId:String,badgeInfo:Object})
 
-const dbupdatecron=mongoose.model('hackerrankdbs',hidUpdate,'hackerrankdbs')
+//const hidUpdate=new mongoose.Schema({hackerRankId:String,badgeInfo:Object})
+
+
+//const dbupdatecron=mongoose.model('hackerrankdbs',hidUpdate,'hackerrankdbs')
+const dbupdatecron=require('../models/saveinfo')
+
 let url='https://hackerrank-badges.herokuapp.com/api/'
 const axios=require('axios')
-let hids=[]
 
-    dbupdatecron.find({},{hackerRankId:1,_id:1}).then(data=>{
+cron.schedule('30 0 * * *',dbupdate) //DAILY updates at 12:30 AM UTC (6:00 AM IST)
+
+    function dbupdate(){
+        console.log("Update started :" + new Date())
+        dbupdatecron.find({},{hackerRankId:1,badgeInfo:1,_id:1}).then(data=>{
 
         var i = 0;
         const l=data.length
         var intervalId = setInterval(function(){
            if(i == l-1){
+              
+            
               clearInterval(intervalId);
            }
             let a=data[i]
             let newurl=url+a.hackerRankId
-            console.log(a._id)
+            
             axios.get(newurl).then(badgeData=>{
-                console.log(newurl)
+                
                 return badgeData.data
 
             }).then(data=>{
-                console.log(data)
-               dbupdatecron.findByIdAndUpdate(
-                   a._id,
-                   {$set:
-                    {
-                        'badgeInfo':data
-                    }
-                   },function(err, doc) {
-                    console.log(doc);
+                if(data.status!="ERROR" && !_.isEqual(data,a.badgeInfo)){
+                    dbupdatecron.findByIdAndUpdate(
+                        a._id,
+                        {$set:
+                         {
+                             'badgeInfo':data
+                             
+                         }
+                        },
+                        {useFindAndModify:false,new:true}
+                        ,function(err, doc) {
+                         console.log(a.hackerRankId + " Updated at " + new Date());
+                         
+                     },
+                     
+                     
+                        )
                 }
-                   )
+                
+               
+            }).catch(err=>{
+                console.log(err)
             })
            i++;
         }, 5000);
-
-
-
-            
         
+    }
+    )
     
-    })
+}
+
     
       
 
