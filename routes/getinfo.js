68ac36fdc3c67bfe1d76bdc1b9c250f882322876
BharatRathore <bsrathore0099@gmail.com>
2020-05-13 20:17:29 +0530

    fixed auth bugs

--- routes/getinfo.js
+++ routes/getinfo.js
@@ -3,14 +3,20 @@ const router=express.Router()
 const userInfo=require('../models/saveinfo')
 
 
-router.get('/',(req,res)=>{
-    userInfo.find()
+router.get('/:key',(req,res)=>{
+    if(req.params.key==proces.env.key){
+        userInfo.find()
     .then(alldata=>{
         res.json(alldata)
     })
     .catch(err=>{
         console.log(err)
     })
+    }
+    else{
+        res.send("Unauthorized...")
+    }
+    
 })
 
 module.exports=router
